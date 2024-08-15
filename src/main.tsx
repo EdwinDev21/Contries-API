8b8ab5556cb377d67d7e4d6720049baae240e146
Edwin ayerbe <edwinayerbe38@gmail.com>
2024-08-15 21:34:16 +0100

    4

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Root from "./routes/Root.tsx";
 import CountryInf from "./routes/CountryInf.tsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-  },
-  {
-    path: "/country-info",
-    element: <CountryInf />,
-  },
-]);
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <ChakraProvider>
     <React.StrictMode>
-      <RouterProvider router={router} />
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/country-info" element={<CountryInf />} />
+        </Routes>
+      </HashRouter>
     </React.StrictMode>
   </ChakraProvider>
 );
